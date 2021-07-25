9b96ef1143d15533482764a5ba487d05719ca791
maru-256 <maru043@docomo.ne.jp>
2021-07-25 21:06:33 +0900

    LaserのApiを実装

--- src/main/java/net/kunmc/lab/runawayfromlaser/task/GenerateLaserTask.java
+++ src/main/java/net/kunmc/lab/runawayfromlaser/task/GenerateLaserTask.java
@@ -8,26 +8,41 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class GenerateLaserTask extends BukkitRunnable {
-    final Location origin;
-    final int length;
+    Location origin;
+    int length;
+    double gap = 0.0625;
+    float laserSize = 1.0F;
+    MoveLaserTask moveLaserTask;
+    DetectHitPlayerTask detectHitPlayerTask;
 
     public GenerateLaserTask(Location origin, int length) {
         this.origin = origin.clone().add(0, 2.0, 0);
         this.length = length;
     }
 
-    public void execute() {
+    public LaserApi laserApi() {
+        return new LaserApi(this);
+    }
+
+    public LaserApi execute() {
         JavaPlugin plugin = RunAwayFromLaser.getInstance();
+
         this.runTaskTimerAsynchronously(plugin, 0, 0);
-        new MoveLaserTask(this).runTaskTimerAsynchronously(plugin, 0, 0);
-        new DetectHitPlayerTask(this).runTaskTimer(plugin, 0, 4);
+
+        moveLaserTask = new MoveLaserTask(this);
+        moveLaserTask.runTaskTimerAsynchronously(plugin, 0, 0);
+
+        detectHitPlayerTask = new DetectHitPlayerTask(this);
+        detectHitPlayerTask.runTaskTimer(plugin, 0, 4);
+
+        return new LaserApi(this);
     }
 
     @Override
     public void run() {
-        for (double x = -2; x < length + 2; x += 0.0625) {
+        for (double x = -2; x < length + 2; x += gap) {
             origin.getWorld().spawnParticle(Particle.REDSTONE, origin.getX() + x, origin.getY(), origin.getZ(),
-                    1, 0, 0, 0, 0, new Particle.DustOptions(Color.RED, 1.0F));
+                    1, 0, 0, 0, 0, new Particle.DustOptions(Color.RED, laserSize));
         }
     }
 }
