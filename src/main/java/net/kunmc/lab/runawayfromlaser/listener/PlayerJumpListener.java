eba2d2ddb641ae1bdba3e00f989365684b724768
maru-256 <maru043@docomo.ne.jp>
2021-08-04 00:35:22 +0900

    ジャンプキャンセル処理修正

--- src/main/java/net/kunmc/lab/runawayfromlaser/listener/PlayerJumpListener.java
+++ src/main/java/net/kunmc/lab/runawayfromlaser/listener/PlayerJumpListener.java
@@ -2,6 +2,7 @@ package net.kunmc.lab.runawayfromlaser.listener;
 
 import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import net.kunmc.lab.runawayfromlaser.GameManager;
+import net.kunmc.lab.runawayfromlaser.util.Util;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,6 +10,10 @@ import org.bukkit.event.Listener;
 public class PlayerJumpListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJump(PlayerJumpEvent e) {
+        if (Util.isCreativeOrSpectator(e.getPlayer())) {
+            return;
+        }
+
         if (GameManager.getInstance().isStarted) {
             e.setCancelled(true);
         }
